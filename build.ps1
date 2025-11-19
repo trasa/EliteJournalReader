b7a565712713716bbb3b0153ae06a5d128328ec0
Tony Rasa <trasa@meancat.com>
2025-11-18 22:01:36 -0800

    build script

---
+++ build.ps1
@@ -0,0 +1,20 @@
+﻿$ErrorActionPreference = "Stop"
+
+$sdkProjectPath = "EliteJournalReader/EliteJournalReader.csproj"
+$distDir = "dist"
+$nupkgOutDir = "$distDir/nupkg"
+
+# clean
+Remove-Item $nupkgOutDir -Recurse -Force -ErrorAction SilentlyContinue
+
+# rebuild dirs
+New-Item -ItemType Directory -Path $nupkgOutDir | Out-Null
+
+dotnet build $sdkProjectPath --configuration Release
+
+dotnet pack $sdkProjectPath `
+    --configuration Release `
+    --output $nupkgOutDir `
+    /p:PackageId="EliteJournalReader" `
+    /p:IncludeSymbols=false `
+    /p:IncludeSource=false
