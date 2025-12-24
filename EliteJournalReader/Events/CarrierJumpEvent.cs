85827ab1a0c76edfee1f6b8e681cf12b80187779
Tony Rasa <trasa@meancat.com>
2025-12-23 16:43:46 -0800

    switch main project to netstandard21 (and other fixes)
    so I can use this dll in unity6

--- EliteJournalReader/Events/CarrierJumpEvent.cs
+++ EliteJournalReader/Events/CarrierJumpEvent.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
-using System.Security.Policy;
+
 
 namespace EliteJournalReader.Events
 {
