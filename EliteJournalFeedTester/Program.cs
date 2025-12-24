e1bee184f6040645548e8c8bd4aae0623577c1f5
Tony Rasa <trasa@meancat.com>
2025-12-23 16:48:27 -0800

    update build for unity6 / net10

--- EliteJournalFeedTester/Program.cs
+++ EliteJournalFeedTester/Program.cs
@@ -1,10 +1,7 @@
 ﻿using EliteJournalReader;
 using EliteJournalReader.Events;
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EliteJournalFeedTester
 {
